25fc8fdd4c15168e4b174d6ae6840fe0c9bda116
Liam <31185529+liamtw22@users.noreply.github.com>
2025-09-18 11:45:55 -0400

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,25 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
+const production = !process.env.ROLLUP_WATCH;
+
 export default {
   input: 'src/room-card.ts',
   output: {
     file: 'room-card.js',
-    format: 'es'
+    format: 'es',
+    sourcemap: !production
   },
   plugins: [
     resolve(),
-    typescript(),
-    terser({
+    typescript({
+      sourceMap: !production,
+      inlineSources: !production
+    }),
+    production && terser({
       format: {
         comments: false
       }
     })
-  ]
+  ].filter(Boolean)
 };
