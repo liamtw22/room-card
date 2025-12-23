916f4da4e2b1115cfef8557c165c13b673cd696c
Liam <31185529+liamtw22@users.noreply.github.com>
2025-12-22 21:03:23 -0500

    Update CARD_VERSION to 2.2.0

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const CARD_VERSION = '2.1.0';
+export const CARD_VERSION = '2.2.0';
 export const CARD_NAME = 'Room Card';
 
 // Use HA theme variables with fallbacks (Material You compatible)
